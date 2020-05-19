9fabde563d0fb2dfd81225f53714841127d40da9
Adelina Lintuluoto <adelina.eleonora.lintuluoto@cern.ch>
2020-05-19 15:58:26 +0100

    Updates

---
+++ precache-manifest.9068749d02f757f89233c13554014a3d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "440ec9a77cfffe5a59937fc51fe52309",
+    "url": "/cmssw-docker-frontend/index.html"
+  },
+  {
+    "revision": "aa290a13e08560d36836",
+    "url": "/cmssw-docker-frontend/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "482545e1d7a055fa6c5e",
+    "url": "/cmssw-docker-frontend/static/js/2.87609391.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cmssw-docker-frontend/static/js/2.87609391.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa290a13e08560d36836",
+    "url": "/cmssw-docker-frontend/static/js/main.e3061fc8.chunk.js"
+  },
+  {
+    "revision": "e72bf67b3f4aae855300",
+    "url": "/cmssw-docker-frontend/static/js/runtime-main.f8ef7bc9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cmssw-docker-frontend/static/media/logo.5d5d9eef.svg"
+  }
+]);
